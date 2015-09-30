931d93b930466a17adadd6a0e891775e44a795b0
Sabrina Tee <sabrinatee@me.com>
2015-09-30 13:29:46 -0700

    This is the second commit that includes error correction.

--- game.js
+++ game.js
@@ -3,10 +3,10 @@ console.log(username);
 
 alert("Welcome "+username+"!");
 
-var question1 = prompt("Am I from Malaysia?").toUpperCase();
+var question1 = prompt("Am I from Malaysia?");
 console.log("Question 1 Response: " + question1);
 
-if (question1 === 'YES') {
+if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
   console.log('Question 1 correct.');
   alert('Rejoice! ' + username + ', you answered Question 1 correctly...I AM from Malaysia!');
 } else {
@@ -14,10 +14,10 @@ if (question1 === 'YES') {
   alert("No "+ username + ", you got Question 1 wrong...I AM from Malaysia!");
 };
 
-var question2 = prompt("Have I lived in Seattle for years?").toUpperCase();
+var question2 = prompt("Have I lived in Seattle for years?");
 console.log("Question 2 Response: " + question2);
 
-if (question2 === 'NO') {
+if (question2.toUpperCase() === 'NO' || question2.toUpperCase() === 'N') {
   console.log('Question 2 correct.');
   alert('Rejoice! ' + username + ', you answered Question 2 correctly...I just moved to Seattle!');
 } else {
@@ -25,22 +25,15 @@ if (question2 === 'NO') {
   alert("No "+ username + ", you got Question 2 wrong...I just moved to Seattle!");
 };
 
-var question3 = prompt("Do I surf?").toUpperCase();
+var question3 = prompt("Do I surf?");
 console.log("Question 3 Response: " + question3);
 
-if (question3 === 'YES') {
+if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
   console.log('Question 3 correct.');
   alert('Rejoice! ' + username + ', you answered Question 3 correctly...I LOVE to surf!');
 } else {
   console.log('Question 3 wrong.');
   alert("No "+ username + ", you got Question 3 wrong...I LOVE to surf!");
 };
-
-alert("Your answer to the Question 1 was " + question1 + ".");
-
-alert("Your answer to the Question 2 was " + question2 + ".");
-
-alert("Your answer to the Question 3 was " + question3 + ".");
-
 
 alert("Thanks for playing my little guessing game!");
