e19316bc80c2e93db22ee45785a8138a16544424
Sabrina Tee <sabrinatee@me.com>
2015-09-30 13:12:54 -0700

    This is the first commit that responds to the user with feedback.

--- game.js
+++ game.js
@@ -6,11 +6,35 @@ alert("Welcome "+username+"!");
 var question1 = prompt("Am I from Malaysia?").toUpperCase();
 console.log("Question 1 Response: " + question1);
 
+if (question1 === 'YES') {
+  console.log('Question 1 correct.');
+  alert('Rejoice! ' + username + ', you answered Question 1 correctly...I AM from Malaysia!');
+} else {
+  console.log('Question 1 wrong.');
+  alert("No "+ username + ", you got Question 1 wrong...I AM from Malaysia!");
+};
+
 var question2 = prompt("Have I lived in Seattle for years?").toUpperCase();
 console.log("Question 2 Response: " + question2);
 
+if (question2 === 'NO') {
+  console.log('Question 2 correct.');
+  alert('Rejoice! ' + username + ', you answered Question 2 correctly...I just moved to Seattle!');
+} else {
+  console.log('Question 2 wrong.');
+  alert("No "+ username + ", you got Question 2 wrong...I just moved to Seattle!");
+};
+
 var question3 = prompt("Do I surf?").toUpperCase();
 console.log("Question 3 Response: " + question3);
+
+if (question3 === 'YES') {
+  console.log('Question 3 correct.');
+  alert('Rejoice! ' + username + ', you answered Question 3 correctly...I LOVE to surf!');
+} else {
+  console.log('Question 3 wrong.');
+  alert("No "+ username + ", you got Question 3 wrong...I LOVE to surf!");
+};
 
 alert("Your answer to the Question 1 was " + question1 + ".");
 
@@ -18,25 +42,5 @@ alert("Your answer to the Question 2 was " + question2 + ".");
 
 alert("Your answer to the Question 3 was " + question3 + ".");
 
-if (question1 === 'YES') {
-  console.log('Question 1 answered correctly.');
-} else {
-  console.log('Question 1 wrong.');
-  alert("No, you got Question 1 wrong...I AM from Malaysia!");
-};
-
-if (question2 === 'NO') {
-  console.log('Question 2 answered correctly.');
-} else {
-  console.log('Question 2 wrong.');
-  alert("No, you got Question 2 wrong...I just moved to Seattle!");
-};
-
-if (question3 === 'YES') {
-  console.log('Question 3 answered correctly.');
-} else {
-  console.log('Question 3 wrong.');
-  alert("No, you got Question 3 wrong...I LOVE to surf!");
-};
 
 alert("Thanks for playing my little guessing game!");
