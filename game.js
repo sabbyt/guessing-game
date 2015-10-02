64e75a69ac4fe6e048ed8d01b3e46b84aa49f16c
Austin Sprague <Sprague@Austins-MacBook-Pro.local>
2015-10-02 15:20:44 -0700

    added css styling

--- game.js
+++ game.js
@@ -17,10 +17,12 @@ function ques1(){
   console.log("Question 1 User Response: " + question1);
   if (question1.toUpperCase() === answers[0] || question1.toUpperCase() === answers[1]) {
   console.log('Question 1 correct.');
+  one.className = 'correct';
   one.innerHTML = 'Rejoice! ' + username + ', you answered Question 1 correctly...I AM from Malaysia!' + "<p><img src= 'img/pokemon.png' width = '200' height ='200'/> </p>";
   correct++;
   } else {
   console.log('Question 1 wrong.');
+  one.className = 'incorrect';
   one.innerHTML = "No "+ username + ", you got Question 1 wrong...I AM from Malaysia!";
   }
 
@@ -31,9 +33,11 @@ function ques2(){
   console.log("Question 2 User Response: " + question2);
   if (question2.toUpperCase() === answers[2] || question2.toUpperCase() === answers[3]) {
   console.log('Question 2 correct.');
+  two.className = 'correct';
   two.innerHTML = 'Rejoice! ' + username + ', you answered Question 2 correctly...I just moved to Seattle!' + "<p><img src= 'img/pokemon.png' width = '200' height ='200'/> </p>";
   correct++;
   } else {
+  two.className = 'incorrect';
   console.log('Question 2 wrong.');
   two.innerHTML = "No "+ username + ", you got Question 2 wrong...I just moved to Seattle!";
   }
@@ -44,10 +48,12 @@ function ques3(){
   var question3 = prompt(questions[2]);
   console.log("Question 3 User Response: " + question3);
   if (question3.toUpperCase() === answers[0] || question3.toUpperCase() === answers[1]) {
+  three.className ='correct';
   console.log('Question 3 correct.');
   three.innerHTML = 'Rejoice! ' + username + ', you answered Question 3 correctly...I LOVE to surf!' + "<p><img src= 'img/pokemon.png' width = '200' height ='200'/> </p>";
   correct++;
   } else {
+  three.className ='incorrect';
   console.log('Question 3 wrong.');
   three.innerHTML = "No "+ username + ", you got Question 3 wrong...I LOVE to surf!";
   }
