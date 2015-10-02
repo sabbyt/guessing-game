2f490c2d6a205e2d4ba9ab3b4ec2761f7b5e87f2
Austin Sprague <Sprague@Austins-MacBook-Pro.local>
2015-10-02 13:55:16 -0700

    move questions and answers into arrays

--- game.js
+++ game.js
@@ -4,15 +4,18 @@ document.getElementById("two");
 document.getElementById("three");
 document.getElementById("four");
 
+var questions = ["Am I from Malaysia?", "Have I lived in Seattle for years?", "Do I surf?"];
+var answers = ["YES","Y","NO","N"];
+
 var username = prompt("What is your name?").toUpperCase();
 console.log("username: "+username);
 welcome.innerHTML="Welcome "+username+"!";
 var correct = 0;
 
 function ques1(){
-  var question1 = prompt("Am I from Malaysia?");
+  var question1 = prompt(questions[0]);
   console.log("Question 1 User Response: " + question1);
-  if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
+  if (question1.toUpperCase() === answers[0] || question1.toUpperCase() === answers[1]) {
   console.log('Question 1 correct.');
   one.innerHTML = 'Rejoice! ' + username + ', you answered Question 1 correctly...I AM from Malaysia!';
   correct++;
@@ -24,9 +27,9 @@ function ques1(){
 }
 
 function ques2(){
-  var question2 = prompt("Have I lived in Seattle for years?");
+  var question2 = prompt(questions[1]);
   console.log("Question 2 User Response: " + question2);
-  if (question2.toUpperCase() === 'NO' || question2.toUpperCase() === 'N') {
+  if (question2.toUpperCase() === answers[2] || question2.toUpperCase() === answers[3]) {
   console.log('Question 2 correct.');
   two.innerHTML = 'Rejoice! ' + username + ', you answered Question 2 correctly...I just moved to Seattle!';
   correct++;
@@ -38,9 +41,9 @@ function ques2(){
 }
 
 function ques3(){
-  var question3 = prompt("Do I surf?");
+  var question3 = prompt(questions[2]);
   console.log("Question 3 User Response: " + question3);
-  if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
+  if (question3.toUpperCase() === answers[0] || question3.toUpperCase() === answers[1]) {
   console.log('Question 3 correct.');
   three.innerHTML = 'Rejoice! ' + username + ', you answered Question 3 correctly...I LOVE to surf!';
   correct++;
