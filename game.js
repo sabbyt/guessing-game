0aba5c4c8cd1d6fa079cfc169190500349eeef78
Sabrina Tee <sabrinatee@me.com>
2015-10-02 18:25:56 -0700

    working counter

--- game.js
+++ game.js
@@ -16,10 +16,10 @@ function ques1(){
   var question1 = prompt(questions[0]);
   console.log("Question 1 User Response: " + question1);
   if (question1.toUpperCase() === answers[0] || question1.toUpperCase() === answers[1]) {
+    correct++ ;
     console.log('Question 1 correct.');
     one.className = 'correct';
-    one.innerHTML = 'Rejoice! ' + username + ', you answered Question 1 correctly...I AM from Malaysia!' + "<p><img src= 'img/pokemon.png' width = '200' height ='200'/> </p>";
-    correct++;
+    one.innerHTML = 'Rejoice! ' +username+ ', you answered Question 1 correctly...I AM from Malaysia!' + "<p><img src= 'img/pokemon.png' width = '200' height ='200'/> </p>";
   } else {
     console.log('Question 1 wrong.');
     one.className = 'incorrect';
@@ -31,10 +31,10 @@ function ques2(){
   var question2 = prompt(questions[1]);
   console.log("Question 2 User Response: " + question2);
   if (question2.toUpperCase() === answers[2] || question2.toUpperCase() === answers[3]) {
+    correct++ ;
     console.log('Question 2 correct.');
     two.className = 'correct';
     two.innerHTML = 'Rejoice! ' + username + ', you answered Question 2 correctly...I just moved to Seattle!' + "<p><img src= 'img/pokemon.png' width = '200' height ='200'/> </p>";
-    correct++;
   } else {
     two.className = 'incorrect';
     console.log('Question 2 wrong.');
@@ -46,10 +46,10 @@ function ques3(){
   var question3 = prompt(questions[2]);
   console.log("Question 3 User Response: " + question3);
   if (question3.toUpperCase() === answers[0] || question3.toUpperCase() === answers[1]) {
+    correct++ ;
     three.className ='correct';
     console.log('Question 3 correct.');
     three.innerHTML = 'Rejoice! ' + username + ', you answered Question 3 correctly...I LOVE to surf!' + "<p><img src= 'img/pokemon.png' width = '200' height ='200'/> </p>";
-    correct++;
   } else {
     three.className ='incorrect';
     console.log('Question 3 wrong.');
@@ -57,12 +57,11 @@ function ques3(){
   }
 }
 
-ques1();
-window.setTimeout(ques2, 1000);
-window.setTimeout(ques3, 1000);
-
 function summary() {
   four.innerHTML = "You just got " + correct + " out of 3 questions correct! " + username + ", thanks for playing my little guessing game!";
 }
 
-summary();
+ques1();
+window.setTimeout(ques2, 1000);
+window.setTimeout(ques3, 1000);
+window.setTimeout(summary, 1000);
